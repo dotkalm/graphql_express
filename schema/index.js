38bf552fb8e437521fea806cbe36c19ef089facf
dotkalm <joel@joelholmberg.com>
2020-03-24 11:40:55 -0700

    getting the array

--- schema/index.js
+++ schema/index.js
@@ -1,22 +1,37 @@
 const graphql = require ('graphql');  
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('graphql1.sqlite');
-
+const myKids = new graphql.GraphQLObjectType({
+    name: 'child',
+    fields: () => {
+        return{
+            name:{
+                type: graphql.GraphQLString
+            }
+        }
+    }
+})
 const queryType = new graphql.GraphQLObjectType({  
   name: 'Query',
     fields: () => {
         return {
             kids: {
-                type: graphql.GraphQLString,
+                type: new graphql.GraphQLList(myKids),
                 resolve: () => {
-                    return new Promise((res,rej) => {
-                        db.each('SELECT * FROM users',(err,row)=>{
+                    return new Promise(
+                        (resolve,reject) => {
+                        const kidsArray = new Array
+                        db.each('SELECT * FROM users',
+                            (err,row)=>{
                             if(err){
                                 throw err
                             }
-                            console.log(row.name)
-                            res(row.name)
+                            kidsArray.push(row)
                         })
+                        setTimeout(() => {
+                            console.log(kidsArray)
+                            resolve(kidsArray)
+                        }, 10)
                     }) 
                 }
             }
