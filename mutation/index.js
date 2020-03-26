b916f79b77e19cc17599c01d7cbde064769243db
dotkalm <joel@joelholmberg.com>
2020-03-26 13:18:49 -0700

    remove table

--- mutation/index.js
+++ mutation/index.js
@@ -1,15 +1,27 @@
 const graphql = require('graphql');
 const { GraphQLString, GraphQLNonNull, GraphQLID, GraphQLObjectType } = graphql;
+const { myKids } = require('../types')
+
+const removeOffspring = async (name) => {
+    const pool = new Pool(dbConfig)
+    const client = await pool.connect()
+    try{
+        return client.query(`DELETE FROM kids WHERE name = ${name}`)
+    } finally{
+        client.release()
+    }
+}
 
 const Mutation = new GraphQLObjectType({
     name: 'Mutation',
     fields: () => ({
         deleteChild: {
+            type: myKids,
             args: {
-                name: {}
+                name: { type: new GraphQLNonNull(GraphQLString) }
             },
             resolve(parent, args){
-                return 
+                return removeOffSpring(args) 
             }
         }
     })
