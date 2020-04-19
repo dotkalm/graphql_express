5fdc733bc055d688d0e2d4c09e7cf9712baecd0a
dotkalm <joel@joelholmberg.com>
2020-04-19 08:38:40 -0700

    hashes password

--- mutation/users.js
+++ mutation/users.js
@@ -1,7 +1,8 @@
 const bcrypt    = require('bcrypt');
 
 const addUser = async (args) => {
-    const hashedPassword = bcrypt.hashSync('password', bcrypt.genSaltSync(10));
+    console.log(args.username)
+    const hashedPassword = bcrypt.hashSync(args.hashedPassword, bcrypt.genSaltSync(12));
     console.log( hashedPassword )
     return 'add user'
 }
