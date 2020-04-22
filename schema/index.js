c082d4f45df57c5ab7c85b81cab7bda51a8ad208
dotkalm <joel@joelholmberg.com>
2020-04-21 21:23:39 -0700

    req.session logged

--- schema/index.js
+++ schema/index.js
@@ -103,9 +103,7 @@ const RootQuery = new graphql.GraphQLObjectType({
                 },
                 resolve: (root, args, context) => {
                     const { username, password } = args
-                    console.log(context.session.logged)
-                    let user = ''
-                    return checkAuth(dbConfig, username, password)
+                    return checkAuth(dbConfig, username, password, context)
                 }
             }
         }
--- schema/users.js
+++ schema/users.js
@@ -4,7 +4,6 @@ const Pool = require('pg-pool')
 const checkAuth = async (dbConfig, username, password, context) => {
     const pool = new Pool(dbConfig)
     const client = await pool.connect()
-    console.log(username, password)
     try{
         const result = await client.query(`
             SELECT * FROM users
@@ -15,7 +14,9 @@ const checkAuth = async (dbConfig, username, password, context) => {
         }else{
             const passwordsMatch = bcrypt.compareSync(password, userObj.hashedpassword)
             if(passwordsMatch){
-                console.log(result.rows[0])
+                context.req.session.logged = true
+                console.log(context.req.session)
+
                 return result.rows
             }else{
                 throw new Error("wrong password") 
